f7919232c4bf8d462e13292d3b614d3d8098df3c
Emiliano Acosta <emilianoam167@gmail.com>
2026-06-17 19:03:59 -0300

    feat: implementacion HashMap para attributtes sesion

--- YEMA/src/framework_controllers/BaseController.java
+++ YEMA/src/framework_controllers/BaseController.java
@@ -2,15 +2,22 @@ package framework_controllers;
 import dtos.Command;
 import dtos.Response;
 import interfaces.ICommunicator;
+import utils.SessionData;
 
 public abstract class BaseController{
+	protected SessionData sessionData;
 	
+	public void setSessionData(SessionData sessionData) {
+		this.sessionData = sessionData;
+	}
 	/*PORQUE TODOS LOS CONTROLADORES VAN A NECESITAR UN COMUNICADOR, LE PASO
 	 * LA INTERFAZ PARA QUE CONOZCA EL CONTRATO, LUEGO EN MAIN LE ENTREGO
 	 * EL COMUNICADORCONSOLA */
 	// no hace falta comunicador
 	// protected ICommunicator communicator; 
 	public abstract Response Ejecutar(Command comand);
+	
+	
 	
 	
 }
